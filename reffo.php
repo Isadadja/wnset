84244f99b39f835eb97d9888d37a37791c99fa10
isadadja <106791139+Isadadja@users.noreply.github.com>
2022-06-03 12:02:30 +0700

    Add files via upload

---
+++ reffo.php
@@ -0,0 +1,5 @@
+<?php
+$link = "https://app.oneaset.co.id/finance/Finance/LandingPage?channel=web_OneAset_activity_financeinvite&referrerCode=ITXWZ&source=outside&ad=Ym09MiZjcD01Jmd1PW51bGwmdWM9MTAmdWU9MCZ1ZD02NDQ3MTY=";
+$reff = "ITXWZ";
+
+?>
